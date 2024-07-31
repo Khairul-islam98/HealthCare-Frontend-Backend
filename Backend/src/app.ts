858c3e0edd94057207f133894dbec86c9a240e34
khairul-islam98 <janeyrahman900@gmail.com>
2024-08-01 03:50:25 +0600

    backend structure ready

---
+++ Backend/src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import router from './app/routes';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome To ');
+});
+
+// global error handler
+app.use(globalErrorHandler);
+
+// Api Not Found
+app.use(notFound);
+
+export default app;
---
+++ Backend/src/server.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import app from './app';
+import { Server } from 'http';
+import config from './app/config';
+
+let server: Server;
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
