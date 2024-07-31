6ac3be4cacb20c721d7ddc74ae0f025e164bf37d
khairul-islam98 <janeyrahman900@gmail.com>
2024-08-01 03:57:08 +0600

    create config file

---
+++ Backend/src/app/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  twilio_account_sid: process.env.TWILIO_ACCOUNT_SID,
+  twilio_auth_token: process.env.TWILIO_AUTH_TOKEN,
+  twilio_phone_number: process.env.TWILIO_PHONE_NUMBER,
+};
