f9c92896f7df3a695d1f28e3dfaa2b28e3799869
khairul-islam98 <janeyrahman900@gmail.com>
2024-08-01 04:19:43 +0600

    create quary builder

---
+++ Backend/src/app/builder/QueryBuilder.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-prototype-builtins */
+import { FilterQuery, Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+  search(searchableFields: string[]) {
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
+
+    return this;
+  }
+  filter() {
+    const queryObj = { ...this.query }; // copy
+    // Filtering
+    const excludeField = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+    excludeField.forEach((el) => delete queryObj[el]);
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+    return this;
+  }
+  sort() {
+    const sort =
+      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
+    this.modelQuery = this.modelQuery.sort(sort as string);
+    return this;
+  }
+}
+
+export default QueryBuilder;
